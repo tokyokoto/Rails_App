7c11348698bc059a8e88c298b40865ec3ab80793
EC2 Default User <ec2-user@ip-172-31-41-170.ap-southeast-1.compute.internal>
2022-06-19 09:22:20 +0000

    004 commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_current_user, {only: [:index, :show, :edit, :update]}
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  
   def index
     @users = User.all
   end
@@ -8,7 +11,32 @@ class UsersController < ApplicationController
   end
   
   def login_form
-    
+  end
+  
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/users/index")
+    else
+      @error_message = "パスワードまたはメールアドレスが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
+  
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
   end
   
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get "users/index" => "users#index"
-  get "users/:id" => "users#show"
   get "login" => "users#login_form"
+  get "users/:id" => "users#show"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  get "users/:id/edit" => "users#edit"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
